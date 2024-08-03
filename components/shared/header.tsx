99edaf1e3c95a02603a8f7d37c2763013c803834
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2024-08-03 14:18:31 +0500

    Fix typo in header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -12,11 +12,11 @@ interface Props {
 
 export const Header: React.FC<Props> = ({ className }) => {
     return (
-        <header className={cn('border border-b', className)}>
+        <header className={cn('border-b', className)}>
             <Container className="flex items-center justify-between py-8">
                 {/** Left Part */}
                 <Link href="/">
-                    <div className="flext items-center gap-4">
+                    <div className="flex items-center gap-4">
                         <Image src="/logo.png" alt="logo" width={35} height={35}/>
                         <div>
                             <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
