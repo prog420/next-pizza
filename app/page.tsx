2726e6849cde86c6faa34f3e33cbb45de31f145f
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2024-08-03 17:00:32 +0500

    Add sort popup element

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Container } from "@/components/shared";
+import { Categories } from "@/components/shared/categories";
+import { SortPopup } from "@/components/shared/sort-popup";
+import { Title } from "@/components/shared/title";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div></div>
-  );
+  return <> 
+    <Container className="mt-10">
+      <Title text="Все пиццы" size="lg" className="font-extrabold"/>
+      <Categories/>
+      <SortPopup/>
+    </Container>
+  </>;
 }
