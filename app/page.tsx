cfb88838f5fee2ef701566d5ea86b0d4b81aeb69
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2024-08-03 18:28:23 +0500

    Add filter checkboxes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/components/shared";
 import { Categories } from "@/components/shared/categories";
+import { Filters } from "@/components/shared/filters";
 import { SortPopup } from "@/components/shared/sort-popup";
 import { Title } from "@/components/shared/title";
 import { TopBar } from "@/components/shared/top-bar";
@@ -11,5 +12,19 @@ export default function Home() {
       <Title text="Все пиццы" size="lg" className="font-extrabold" />
     </Container>
     <TopBar />
+    
+    <Container className="mt-10 pb-14">
+      {/** Filters */}
+      <div className="flex gap-[60px]">
+        <Filters />
+      </div>
+      {/** Items list */}
+      <div className="flex-1">
+        <div className="fkex flex-col gap-16">
+          {/* <ProductsGroupList title="Пиццы" items={{1, 2, 3, 4, 5}} />
+          <ProductsGroupList title="Комбо" items={{1, 2, 3, 4, 5}} /> */}
+        </div>
+      </div>
+    </Container>
   </>;
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -4,3 +4,5 @@ export { Title } from "./title";
 export { Categories } from "./categories";
 import { SortPopup } from "./sort-popup";
 import { TopBar } from "./top-bar";
+import { FilterCheckbox } from "./filter-checkbox";
+import { Filters } from "./filters";
