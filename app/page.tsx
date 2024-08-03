ae7d0373b3f30b260050cf9d42fecf1201738403
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2024-08-03 17:11:26 +0500

    Add TopBar element

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import { Container } from "@/components/shared";
 import { Categories } from "@/components/shared/categories";
 import { SortPopup } from "@/components/shared/sort-popup";
 import { Title } from "@/components/shared/title";
+import { TopBar } from "@/components/shared/top-bar";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return <> 
     <Container className="mt-10">
-      <Title text="Все пиццы" size="lg" className="font-extrabold"/>
-      <Categories/>
-      <SortPopup/>
+      <Title text="Все пиццы" size="lg" className="font-extrabold" />
     </Container>
+    <TopBar />
   </>;
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -3,3 +3,4 @@ export { Header } from "./header";
 export { Title } from "./title";
 export { Categories } from "./categories";
 import { SortPopup } from "./sort-popup";
+import { TopBar } from "./top-bar";
