2d66755fbfce9c86d1992c9179f21efe3a3e5396
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2024-08-04 14:53:10 +0500

    Add ProductCard element

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@/components/shared";
 import { Categories } from "@/components/shared/categories";
 import { Filters } from "@/components/shared/filters";
+import { ProductCard } from "@/components/shared/product-card";
 import { SortPopup } from "@/components/shared/sort-popup";
 import { Title } from "@/components/shared/title";
 import { TopBar } from "@/components/shared/top-bar";
@@ -14,15 +15,23 @@ export default function Home() {
     <TopBar />
     
     <Container className="mt-10 pb-14">
-      {/** Filters */}
       <div className="flex gap-[60px]">
-        <Filters />
-      </div>
-      {/** Items list */}
-      <div className="flex-1">
-        <div className="fkex flex-col gap-16">
-          {/* <ProductsGroupList title="Пиццы" items={{1, 2, 3, 4, 5}} />
-          <ProductsGroupList title="Комбо" items={{1, 2, 3, 4, 5}} /> */}
+        {/** Filters */}
+        <div className="w-[250px]">
+          <Filters />
+        </div>
+
+        {/** Items list */}
+        <div className="flex-1">
+          <div className="flex flex-col gap-16">
+            <ProductCard
+              id={1}
+              name="Пепперони фреш"
+              price={500}
+              imageUrl="https://media.dodostatic.net/image/r:584x584/11EEE204813D7DDB85018555332A9F46.avif" />
+            {/* <ProductsGroupList title="Пиццы" items={{1, 2, 3, 4, 5}} />
+            <ProductsGroupList title="Комбо" items={{1, 2, 3, 4, 5}} /> */}
+          </div>
         </div>
       </div>
     </Container>
