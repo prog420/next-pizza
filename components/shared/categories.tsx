1975c02c60f1c51a19121912d111734c6ed13403
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2024-08-04 20:37:20 +0500

    Add category selection on click

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -25,10 +25,14 @@ export const Categories: React.FC<Props> = ({ className }) => {
         <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
             {
                 categories.map(({name, id}, index) => (
-                    <a className={cn(
-                        'flex items-center font-bold h-11 rounded-2xl px-5',
-                        categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary'
-                    )} key={index}>
+                    <a 
+                        className={cn(
+                            'flex items-center font-bold h-11 rounded-2xl px-5',
+                            categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary'
+                        )} 
+                        key={index}
+                        href={`/#${name}`}
+                    >
                         <button>{name}</button>
                     </a>
                 ))
